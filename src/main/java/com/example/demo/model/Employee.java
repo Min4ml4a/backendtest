67efec0b9fb283a5d5147555d9514a0a737e4d7f
Min4Mlna <6310210435@email.psu.ac.th>
2023-09-05 09:05:02 +0700

    ss

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,52 +1,50 @@
 package com.example.demo.model;
 
 public class Employee {
-
 	
-		private Integer employeeId;
-		private String fristname;
-		private String lastname;
-		private Integer salary;
-		
-		
-		
-		public Employee() {
-			super();
-		}
+	private Integer employeeId;
+	private String firstName;
+	private String lastName;
+	private Integer salary;
+	
+	
+	public Employee() {
+		super();
+	}
 
-
-		public Employee(Integer employeeId, String fristname, String lastname, Integer salary) {
-			super();
-			this.employeeId = employeeId;
-			this.fristname = fristname;
-			this.lastname = lastname;
-			this.salary = salary;
-		}
-		
-		
-		public Integer getEmployeeId() {
-			return employeeId;
-		}
-		public void setEmployeeId(Integer employeeId) {
-			this.employeeId = employeeId;
-		}
-		public String getFristname() {
-			return fristname;
-		}
-		public void setFristname(String fristname) {
-			this.fristname = fristname;
-		}
-		public String getLastname() {
-			return lastname;
-		}
-		public void setLastname(String lastname) {
-			this.lastname = lastname;
-		}
-		public Integer getSalary() {
-			return salary;
-		}
-		public void setSalary(Integer salary) {
-			this.salary = salary;
-		}
-		
+	public Employee(Integer employeeId, String firstName, String lastName, Integer salary) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Integer getSalary() {
+		return salary;
+	}
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+	
+	
+	
 }
